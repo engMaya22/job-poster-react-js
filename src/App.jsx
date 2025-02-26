7a44aca2925426e24c487b42a7c1fd6c59bce081
engMaya22 <eng.maya.esmaeel1@gmail.com>
2025-02-26 18:17:30 +0300

    card show functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListing from "./components/JobListing";
 import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   const title = "Become a React Dev";
@@ -12,13 +13,7 @@ const App = () => {
               <Hero  title={title} subtitle={subtitle} />
               <HomeCards />
               <JobListing />             
-                <section className="m-auto max-w-lg my-10 px-6">
-                  <a
-                    href="jobs.html"
-                    className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-                    >View All Jobs</a
-                  >
-                </section>
+              <ViewAllJobs />
              </>
  
 
@@ -28,3 +23,4 @@ const App = () => {
 export default App;
 
 //when using tailwind with react we need to search by vite with tailwind
+//components state , global state 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -4,6 +4,7 @@ import jobs from "../jobs.json";
 import JobCard from "./JobCard";
 
 const JobListing = ()=>{
+   const recentJobs = jobs.slice(0,3);
 
     return ( 
     <section className="bg-blue-50 px-4 py-10">
@@ -13,7 +14,7 @@ const JobListing = ()=>{
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {
-                jobs.map((job , indx)=> <JobCard key={indx} job={job}/>)
+                recentJobs.map((job , indx)=> <JobCard key={indx} job={job}/>)
             }
 
            
